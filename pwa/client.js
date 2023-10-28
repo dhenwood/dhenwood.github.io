fbe7ed679c1a5328d7a40c3571317497bc6f4c05
dhenwood <dhenwood@users.noreply.github.com>
2023-10-29 07:45:22 +1100

    Update client.js

--- pwa/client.js
+++ pwa/client.js
@@ -1,1 +1,19 @@
 
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const myDate = new Date();
+  var myHour = myDate.getHours();
+  var myMinute = myDate.getMinutes();
+  var mySecond = myDate.getSeconds();
+  var ampm = myHour >= 12 ? 'pm' : 'am';
+  myHour = myHour % 12;
+  myHour = myHour ? myHour : 12; // the hour '0' should be '12'
+  myMinute = myMinute < 10 ? '0'+myMinute : myMinute;
+  mySecond = mySecond < 10 ? '0'+mySecond : mySecond;
+  var strTime = myHour + ':' + myMinute + ':' + mySecond + ' ' + ampm;
+
+  timeDisplay.textContent = strTime;
+}
+
+
+setInterval(refreshTime, 1000);
