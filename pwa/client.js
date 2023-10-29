0b6b57679e12838844d825a79936f45e87ea8ac8
dhenwood <dhenwood@users.noreply.github.com>
2023-10-29 15:46:46 +1100

    Update client.js

--- pwa/client.js
+++ pwa/client.js
@@ -1,15 +1,14 @@
-
 //Gets the Device Name
 function getDeviceName() {
         const params = new URLSearchParams(window.location.search);
         const serialNumber = params.get('roomname')
         var serialResult;
-        serialNumber === null ? serialResult = 'peripheralSerial not set' : serialResult = serialNumber
+        serialNumber === null ? serialResult = 'Display name not set' : serialResult = serialNumber
         document.getElementById('roomName').textContent = serialResult;
 }
 
 function refreshTime() {
-  const timeDisplay = document.getElementById("time");
+  //const timeDisplay = document.getElementById("time");
   const myDate = new Date();
   var myHour = myDate.getHours();
   var myMinute = myDate.getMinutes();
@@ -25,27 +24,82 @@ function refreshTime() {
 }
 
 
-//setInterval(refreshTime, 1000);
-//getDeviceName();
 
 async function init() {
         try {
                 //xapi = await getXAPI();
+                //const xapistatus = document.getElementById('xapistatus');
+                document.getElementById('xapistatus').textContent = "jsxapi available";
                 //xapistatus.textContent = "jsxapi available";
                 //myState.textContent = "Occupied";
                 //unique_id = createPersistentCookie();
                 //content.textContent = "Navigator ID: " + unique_id;
-                //setupSubscriptions();
-                //getCurrent();
+                setupSubscriptions();
+                getCurrent();
                 //updateSerial();
-          refreshTime()
+          //refreshTime()
+          setInterval(refreshTime, 1000);
           getDeviceName();
         } catch(e) {
-                content.textContent = e.message;
+                //content.textContent = e.message;
+                //const xapistatus = document.getElementById('xapistatus');
                 //xapistatus.textContent = "error getting jsxapi object";
+                document.getElementById('xapistatus').textContent = 'Error: ' + e.message;
         }
 }
  
 window.onload = async function() {
         init();         
 }; 
+
+const timeDisplay = document.getElementById("time");
+const xapistatus = document.getElementById('xapistatus');
+
+//Gets the current xStatus of LedControl Color and displays on the page.
+function getCurrent() {
+        //Example xapi xStatus
+        xapi.Status.UserInterface.LedControl.Color.get().then((color) => {
+                setLedColor(color)
+    })     
+    .catch(function(error) {
+                console.log(error);
+    });
+}
+
+function setLedColor(color) {
+        console.log("COLOR: " + color)
+        switch(color) {
+                case 'Green':
+                case 'Yellow':
+                case 'Red':
+                case 'Blue':
+                case 'Purple':
+                case 'Orange':
+                         //document.getElementById('roomName').innerHTML = color;
+                        document.getElementById('roomName').style.height ='130px';
+                        document.getElementById('roomName').style.backgroundColor = color;
+                        document.getElementById('roomOccupied').style.height ='130px';
+                        document.getElementById('roomOccupied').style.backgroundColor = color;
+                        if(color == 'Green'){
+                                document.getElementById('roomOccupied').innerHTML = 'Room is free';
+                        }else{
+                                document.getElementById('roomOccupied').innerHTML = 'Room Occupied';
+                        }
+                         break;
+                case 'Off':
+                        document.getElementById('ledRect').style.fill = 'black';
+                        break;
+                default:
+                        console.log("Unexpected color")
+                        document.getElementById('ledRect').style.fill = 'grey';
+        }
+}
+
+
+//Gets the current xStatus of LedControl Color and displays on the page.
+function setupSubscriptions() {
+        //Example xapi xStatus
+        xapi.Status.UserInterface.LedControl.Color.on(color => {
+                setLedColor(color)
+        });
+}
